c4d2a62decfbeff3815dcd62cf481ade118c5f86
simon <rossi.sim@outlook.com>
2020-06-22 18:12:27 +0200

    Add verbose option to program

--- radio_parser.py
+++ radio_parser.py
@@ -6,12 +6,17 @@ from wikipedia.controller import SearchController
 from argparse import ArgumentParser
 
 import sys
+
+VERBOSE = None
+
 def get_options():
     """
-    Syntax: [-h] [--wiki-title title | --csv filename]
+    Syntax: [-h] [--wiki-title title | --csv filename] 
+        [--verbose]
 
     See --help for options details.
     """
+    global VERBOSE
     description = """
 Search radio contact in list of station,
 using wikipedia "list of radio stations" pages.
@@ -25,15 +30,16 @@ and parse them to find contact email."""
     group.add_argument("--csv", metavar="filename",
             help="csv file of radio list")
 
+    parser.add_argument("-v", "--verbose",
+            action="store_true", default=False)
 
-    def err_msg(msg):
-        parser.print_usage()
-        print(msg)
     args = parser.parse_args()
+
+    #Error if no argument is given
     if not args.wiki_title and not args.csv:
         parser.print_usage()
-        err_msg = f"{sys.argv[0]}: error: no argument is given"
-        exit(err_msg)
+        exit(f"{sys.argv[0]}: error: no argument is given")
+    VERBOSE = args.verbose
     return parser.parse_args()
 
 def parse_radio_file(wikilist_file):
@@ -67,7 +73,8 @@ def parse_wiki_list(wikilist_page):
     Store founded website in .template (=csv format) file,
     and use parse_radio_file to explore each radio website.
     """
-    wikisearch = SearchController(wikilist_page, silent=False)
+    silent = VERBOSE == False
+    wikisearch = SearchController(wikilist_page, silent=silent)
     wikisearch.launch()
     parse_radio_file(wikisearch.filename)
 
